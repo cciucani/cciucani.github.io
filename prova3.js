375acec2688833af2b9b5a45cac01689f454e5d9
cciucani <131252260+cciucani@users.noreply.github.com>
2023-12-13 14:00:00 +0100

    Update prova3.js

--- prova3.js
+++ prova3.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat9.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
