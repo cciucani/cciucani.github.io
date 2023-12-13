0370bd3964c349e2cd2a76e8024bcc6a81dea6f6
cciucani <131252260+cciucani@users.noreply.github.com>
2023-12-13 15:16:58 +0100

    Create prova6.js

---
+++ prova6.js
@@ -0,0 +1,84 @@
+define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat9.js'], function(APIConstructor, iatExtension){
+    var API = new APIConstructor();
+
+	return iatExtension({
+		category1 : {
+			name : 'Carriera', //Will appear in the data.
+			title : {
+				media : {word : 'Carriera'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+            {word: 'Carriera'},
+				{word: 'Azienda'},
+				{word: 'Stipendio'},
+				{word: 'Ufficio'},
+				{word: 'Professionista'},
+				{word: 'Management'},
+				{word: 'Business'}
+			], 
+			//Stimulus css (style)
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+		category2 :	{
+			name : 'Famiglia', //Will appear in the data.
+			title : {
+				media : {word : 'Famiglia'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects 
+           	{word: 'Nozze'},
+				{word: 'Matrimonio'},
+				{word: 'Genitori'},
+				{word: 'Parenti'},
+				{word: 'Famiglia'},
+				{word: 'Casa'}, 
+				{word: 'Bambini'} 
+      ], 
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+
+		base_url : {//Where are your images at?
+			image : 'https://baranan.github.io/minno-tasks/images/'
+		},
+		attribute1 :
+			{
+				name : 'Maschio',
+				title : {
+					media : {word : 'Maschio'},
+					css : {color:'#0000FF','font-size':'1.8em'},
+					height : 4 //Used to position the "Or" in the combined block.
+				},
+				stimulusMedia : [ //Stimuli content as PIP's media objects
+{word: 'Marco'},
+				{word: 'Paolo'},
+				{word: 'Daniele'},
+				{word: 'Giovanni'},
+				{word: 'Giuseppe'}
+			],
+				//Stimulus css
+				stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+			},
+			attribute2 :
+			{
+				name : 'Femmina',
+				title : {
+					media : {word : 'Femmina'},
+					css : {color:'#0000FF','font-size':'1.8em'},
+					height : 4 //Used to position the "Or" in the combined block.
+				},
+				stimulusMedia : [ //Stimuli content as PIP's media objects
+          {word: 'Rebecca'},
+				{word: 'Michela'},
+				{word: 'Francesca'},
+				{word: 'Giulia'},
+				{word: 'Anna'}
+			],
+				//Stimulus css
+				stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+			},
+	});
+});
