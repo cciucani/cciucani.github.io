79216378b8f9862155eb22a3323ec98594081c7d
cciucani <131252260+cciucani@users.noreply.github.com>
2023-12-13 16:13:28 +0100

    Update prova7.js

--- prova7.js
+++ prova7.js
@@ -94,7 +94,7 @@ define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat9.js'], function(A
 				'Metti un dito della mano sinistra sul tasto <b>E</b> per le voci che appartengono alla categoria <font color="#336600">leftCategory</font>. ' +
 				'<br/>Metti un dito della mano destra sul tasto <b>I</b> per le voci che appartengono alla categoria <font color="#336600">rightCategory</font>.<br/>' +
 				'Le voci compariranno una alla volta.<br/><br/>' +
-				'Se sbagli apparirà una <font color="#ff0000"><b>X</b></font> rossa. ' +
+				'Se sbagli apparir una <font color="#ff0000"><b>X</b></font> rossa. ' +
 				'Premi l'altro tasto per continuare.<br/>' +
 				'<u>Vai più veloce che puoi </u> cercando di non sbagliare.<br/><br/></p>'+
 				'<p align="center">Premi la <b>barra spaziatrice</b> per iniziare.</font></p></div>',
