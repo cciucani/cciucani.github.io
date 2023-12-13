86127831e45c08eaf2514889833c0756848e62a7
cciucani <131252260+cciucani@users.noreply.github.com>
2023-12-13 15:21:47 +0100

    Update prova6.js

--- prova6.js
+++ prova6.js
@@ -80,5 +80,64 @@ define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat9.js'], function(A
 				//Stimulus css
 				stimulusCss : {color:'#0000FF','font-size':'2.3em'}
 			},
+					leftKeyText : 'Trykk "E" for', 
+			rightKeyText : 'Trykk "I" for', 
+			keysCss : {'font-size':'0.8em', 'font-family':'courier', color:'#000000'},
+			//Text and style for the separator between the top and bottom category labels.
+			orText : 'eller', 
+			orCss : {'font-size':'1.8em', color:'#000000'},
+			finalText : 'Trykk på mellomromstasten for å forstette til neste oppgave',
+			
+		instCategoriesPractice: '<div><p align="center" style="font-size:20px; font-family:arial">' +
+				'<font color="#000000"><u>Del blockNum av nBlocks </u><br/><br/></p>' +
+				'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
+				'Legg venstre finger på <b>E</b> tasten for objekter som tilhører kategorien <font color="#336600">leftCategory</font>. ' +
+				'<br/>Legg høyre finger på <b>I</b> tasten for objekter som tilhører kategorien <font color="#336600">rightCategory</font>.<br/>' +
+				'Ett og ett objekt vil vises om gangen.<br/><br/>' +
+				'Hvis du gjør feil vil en rød <font color="#ff0000"><b>X</b></font> vises. ' +
+				'Trykk på den andre tasten for å fortsette.<br/>' +
+				'<u>Gjennomfør så raskt du kan </u> mens du er så nøyaktig som mulig.<br/><br/></p>'+
+				'<p align="center">Trykk på <b>mellomromstasten</b> når du er klar til å starte.</font></p></div>',
+				
+		instAttributePractice: '<div><p align="center" style="font-size:20px; font-family:arial">' +
+				'<font color="#000000"><u>Del blockNum av nBlocks </u><br/><br/></p>' +
+				'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
+				'Legg venstre finger på <b>E</b> tasten for objekter som tilhører kategorien <font color="#0000ff">leftAttribute.</font>' +
+				'<br/>Legg høyre finger på <b>I</b> tasten for objekter som tilhører kategorien <font color="#0000ff">rightAttribute</font>.<br/><br/>' +
+				'Hvis du gjør feil vil en rød <font color="#ff0000"><b>X</b></font> vises. ' +
+				'Trykk på den andre tasten for å fortsette..<br/>' +
+				'<u>Gjennomfør så raskt du kan </u> mens du er så nøyaktig som mulig..<br/><br/></p>'+
+				'<p align="center">Trykk på <b>mellomromstasten</b> når du er klar til å starte.</font></p></div>',		
+				
+		instFirstCombined : '<div><p align="center" style="font-size:20px; font-family:arial">' +
+				'<font color="#000000"><u>Del blockNum av nBlocks </u><br/><br/></p>' +
+				'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
+				'Bruk <b>E</b> tasten for <font color="#336600">leftCategory</font> og for <font color="#0000ff">leftAttribute</font>.<br/>' +
+				'Bruk <b>I</b> tasten for <font color="#336600">rightCategory</font> og for  <font color="#0000ff">rightAttribute</font>.<br/>' +
+				'Hvert objekt tilhører bare en kategori.<br/><br/>' +
+				'Hvis du gjør feil vil en rød <font color="#ff0000"><b>X</b></font> vises. ' +
+				'Trykk på den andre tasten for å fortsette.<br/>' + 
+				'<u>Gjennomfør så raskt du kan </u> mens du er så nøyaktig som mulig.<br/><br/></p>' +
+				'<p align="center">Trykk på <b>mellomromstasten</b> når du er klar til å starte.</font></p></div>',
+		instSecondCombined : '<div><p align="center" style="font-size:20px; font-family:arial">' +
+				'<font color="#000000"><u>Del blockNum av nBlocks </u><br/><br/></p>' +
+				'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
+				'Dette er det samme som den forrige delen.<br/>' +
+				'Bruk <b>E</b> tasten for <font color="#336600">leftCategory</font> og for <font color="#0000ff">leftAttribute</font>.<br/>' +
+				'Bruk <b>I</b> tasten for <font color="#336600">rightCategory</font> og for  <font color="#0000ff">rightAttribute</font>.<br/>' +
+				'Hvert objekt tilhører bare en kategori.<br/><br/>' +
+				'<u>Gjennomfør så raskt du kan </u> mens du er så nøyaktig som mulig.<br/><br/></p>' +
+				'<p align="center">Trykk på <b>mellomromstasten</b> når du er klar til å starte.</font></p></div>',
+		instSwitchCategories : '<div><p align="center" style="font-size:20px; font-family:arial">' +
+				'<font color="#000000"><u>Del blockNum av nBlocks </u><br/><br/></p>' +
+				'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
+				'<b>Pass på, kategoriene har byttet posisjon!</b><br/>' +
+				'Legg venstre finger på <b>E</b> tasten for <font color="#336600">leftCategory</font>.<br/>' +
+				'Legg høyre finger på <b>I</b> tasten for <font color="#336600">rightCategory</font>.<br/><br/>' +
+				'<u>Gjennomfør så raskt du kan </u> mens du er så nøyaktig som mulig.<br/><br/></p>' +
+				'<p align="center">Trykk på <b>mellomromstasten</b> når du er klar til å starte.</font></p></div>',	
+		remindErrorText : '<p align="center" style="font-size:"0.6em"; font-family:arial">' +
+			'Hvis du gjør feil så vil en rød <font color="#ff0000"><b>X</b></font> vises. ' +
+			'Trykk den andre tasten for å fortsette.<p/>',
 	});
 });
